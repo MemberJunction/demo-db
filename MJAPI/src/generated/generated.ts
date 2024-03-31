6b09122e70b2b98f9c5aa630f3faeccad6a125d2
AN-BC <97354817+AN-BC@users.noreply.github.com>
2024-03-31 17:39:00 -0500

    Upgraded to MJ 1.0 Release Candidate

--- MJAPI/src/generated/generated.ts
+++ MJAPI/src/generated/generated.ts
@@ -2,7 +2,7 @@
 * ALL ENTITIES - TypeGraphQL Type Class Definition - AUTO GENERATED FILE
 * Generated Entities and Resolvers for Server
 * 
-* GENERATED: 3/30/2024, 4:35:30 PM
+* GENERATED: 3/31/2024, 11:58:03 AM
 * 
 *   >>> DO NOT MODIFY THIS FILE!!!!!!!!!!!!
 *   >>> YOUR CHANGES WILL BE OVERWRITTEN
