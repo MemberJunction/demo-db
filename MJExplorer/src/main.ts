8b744ad1a4d231666150cbfb4d5357c3ce220715
hiltongr <hilton@memberjunction.com>
2024-04-04 00:36:43 -0400

    Update main.ts
    Whitespace update
    
    Signed-off-by: hiltongr <hilton@memberjunction.com>

--- MJExplorer/src/main.ts
+++ MJExplorer/src/main.ts
@@ -1,6 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
+
 async function initAndBootstrap() {
   platformBrowserDynamic().bootstrapModule(AppModule)
     .then(ref => {
